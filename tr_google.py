a3dad31fee61457b2bb6258593c06b303e3d3fe3
Aisu Wata <aisu.wata0@gmail.com>
2023-06-02 11:19:00 -0300

    rename vars

--- tr_google.py
+++ tr_google.py
@@ -4,5 +4,5 @@ from googletrans import Translator
 translator = Translator()
 
 def translate(text, src='en', dest='ja') -> str:
-  response_jp = translator.translate(text, src=src, dest=dest)
-  return response_jp.text
+  response = translator.translate(text, src=src, dest=dest)
+  return response.text
