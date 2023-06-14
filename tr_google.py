76238041c0601cb0cb0b157ec72aa93573d744bd
Aisu Wata <aisu.wata0@gmail.com>
2023-06-14 12:47:04 -0300

    tr_google default 'auto'

--- tr_google.py
+++ tr_google.py
@@ -3,6 +3,6 @@ from googletrans import Translator
 
 translator = Translator()
 
-def translate(text, src='en', dest='ja') -> str:
+def translate(text, src='auto', dest='ja') -> str:
   response = translator.translate(text, src=src, dest=dest)
   return response.text
